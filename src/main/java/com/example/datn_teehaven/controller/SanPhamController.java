60345ee603983bcd6fb0bb26cb19a47ca5b7a1be
LuuHuynh21 <dovietluuhuynh@gmail.com>
2024-10-01 21:15:50 +0700

    firebase

--- src/main/java/com/example/datn_teehaven/controller/SanPhamController.java
+++ src/main/java/com/example/datn_teehaven/controller/SanPhamController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/san-pham")
 public class SanPhamController {
-
+//123
     @GetMapping
     public String hienThi(){
         return "admin-template/san_pham/san-pham";
