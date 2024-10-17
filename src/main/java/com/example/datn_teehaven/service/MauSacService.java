cc1692ef03c18131847c26a3904b90db8d2e06b6
Longduong01 <ilivedubai247@gmail.com>
2024-10-17 20:22:22 +0700

    mausac - thuonghieu

--- src/main/java/com/example/datn_teehaven/service/MauSacService.java
+++ src/main/java/com/example/datn_teehaven/service/MauSacService.java
@@ -9,5 +9,23 @@ public interface MauSacService {
 
     List<MauSac> findAll();
 
+    List<MauSac> getAllDangHoatDong();
+
+    List<MauSac> getAllNgungHoatDong();
+
+    void deleteById(Long id);
+
+    MauSac save(MauSac mauSac);
+
+    boolean checkTenTrung(String ten);
+
+    boolean checkTenTrungSua(String ma, String ten);
+
+    MauSac update(MauSac mauSac);
+
+    MauSac getById(Long id);
+
+    Integer genMaTuDong();
+
 
 }
--- src/main/java/com/example/datn_teehaven/service/impl/ThuongHieuServiceImpl.java
+++ src/main/java/com/example/datn_teehaven/service/impl/ThuongHieuServiceImpl.java
@@ -3,7 +3,11 @@ package com.example.datn_teehaven.service.impl;
 
 
 import com.example.datn_teehaven.entyti.ThuongHieu;
+import com.example.datn_teehaven.repository.ThuongHieuRepository;
 import com.example.datn_teehaven.service.ThuongHieuService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +15,72 @@ import java.util.List;
 @Service
 public class ThuongHieuServiceImpl implements ThuongHieuService {
 
+    @Autowired
+    private ThuongHieuRepository thuongHieuRepository;
+
     @Override
     public List<ThuongHieu> findAll() {
-        return null;
+
+        Sort sort = Sort.by(Sort.Direction.DESC, "ngaySua");
+        return thuongHieuRepository.findAll(sort);
+
+    }
+
+    @Override
+    public List<ThuongHieu> getAllDangHoatDong() {
+        return thuongHieuRepository.fillAllDangHoatDong();
+    }
+
+    @Override
+    public List<ThuongHieu> getAllNgungHoatDong() {
+        return thuongHieuRepository.fillAllNgungHoatDong();
+    }
+
+
+    @Override
+    @Transactional
+    public ThuongHieu save(ThuongHieu thuongHieu) {
+
+        return thuongHieuRepository.save(thuongHieu);
+
+    }
+
+    @Override
+    public boolean checkTenTrung(String ten) {
+
+        for (ThuongHieu sp : thuongHieuRepository.findAll()) {
+            if (sp.getTen().equalsIgnoreCase(ten)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    @Override
+    public boolean checkTenTrungSua(Long id, String ten) {
+
+        for (ThuongHieu sp : thuongHieuRepository.findAll()) {
+            if (sp.getTen().equalsIgnoreCase(ten)) {
+                if (!sp.getId().equals(id)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public ThuongHieu update(ThuongHieu thuongHieu) {
+
+        return thuongHieuRepository.save(thuongHieu);
+
+    }
+
+    @Override
+    public ThuongHieu getById(Long id) {
+
+        return thuongHieuRepository.findById(id).get();
+
     }
 }
