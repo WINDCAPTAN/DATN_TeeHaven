69f4d3179472fe58b8aabea674c539bc22fd5ae9
Bachdung204 <bachdung004@gmail.com>
2024-11-13 20:52:21 +0700

    update

--- src/main/java/com/example/datn_teehaven/service/DiaChiService.java
+++ src/main/java/com/example/datn_teehaven/service/DiaChiService.java
@@ -10,6 +10,19 @@ import java.util.List;
 public interface DiaChiService {
     List<DiaChi> getAll();
 
+    List<DiaChi> getAllByTaiKhoan(Long id);
 
+    void deleteById(Long id);
 
+    DiaChi save(DiaChi diaChi);
+
+    boolean checkTenTrung(String ten,Long idTaiKhoan);
+
+    boolean checkTenTrungSua(Long idDiaChi, String ten,Long idTaiKhoan);
+
+    DiaChi update(DiaChi diaChi);
+
+    DiaChi getById(Long id);
+
+    List<DiaChi> getAllTrangThai(Integer trangThai);
 }
